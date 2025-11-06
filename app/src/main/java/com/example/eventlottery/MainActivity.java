7c5e2a49ec753a9f609903f91800d9e715cab14d
AngadSC <angads.chahil@gmail.com>
2025-11-06 16:50:18 -0700

    adding the navigation bar buttons

--- app/src/main/java/com/example/eventlottery/MainActivity.java
+++ app/src/main/java/com/example/eventlottery/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.eventlottery;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +14,20 @@ import com.example.eventlottery.fragments.CreateEventFragment;
 import com.example.eventlottery.fragments.BrowseFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView navBrowse;
+    private TextView navMyEvents;
+    private TextView navScan;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navBrowse = findViewById(R.id.nav_browse);
+        navMyEvents = findViewById(R.id.nav_my_events);
+        navScan = findViewById(R.id.nav_scan);
+
 
         // Load BrowseFragment by default
         if (savedInstanceState == null) {
@@ -26,5 +36,18 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
     }
+
+    private void setupNavigation() {
+        navBrowse.setOnClickListener(v-> {
+            loadFragment(new BrowseFragment());
+            highlightNavButton(navBrowse);
+        });
+
+        navMyEvents.setOnClickListener(v-> {
+
+        });
+    }
+
+
 }
 
