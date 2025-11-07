193e8ad6cd3460812d1e118a31c0dfafb625ee11
AngadSC <angads.chahil@gmail.com>
2025-11-07 13:04:23 -0700

    added more java docs for event history this class is lacking

--- app/src/main/java/com/example/eventlottery/EventHistoryActivity.java
+++ app/src/main/java/com/example/eventlottery/EventHistoryActivity.java
@@ -10,6 +10,14 @@ import com.example.eventlottery.fragments.EventHistoryFragment;
 import com.google.android.material.button.MaterialButtonToggleGroup;
 
 public class EventHistoryActivity extends AppCompatActivity {
+
+    /**
+     * acts as container for displayers events from the user event history
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     /*
      *put the browse, scan, and my events fragment here
      *
@@ -17,6 +25,14 @@ public class EventHistoryActivity extends AppCompatActivity {
 
 
     // This is the corrected onCreate method
+
+    /**
+     * sets the content viw
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     *
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
